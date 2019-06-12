c76a0d41218f9d5a7e6ef04bbab23181ca490332
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-11 20:25:02 -0500

    POST route adds new shift to database

--- server/routes/shift.router.js
+++ server/routes/shift.router.js
@@ -3,15 +3,29 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-//get shifts
+//get all shifts, ordered by start date/time
 router.get('/', (req, res) => {
-    let sqlText = `SELECT * FROM "shifts";`;
-    pool.query(sqlText)
+    let sqlQuery = `SELECT * FROM "shifts" ORDER BY "start" ASC;`;
+    pool.query(sqlQuery)
         .then(result => {
             res.send(result.rows);
         })
         .catch(err => {
             console.log('error from shiftRouter GET:', err);
+            res.sendStatus(500);
+        })
+});
+
+//post a shift
+router.post('/', (req, res) => {
+    let sqlQuery = `INSERT INTO "shifts" ("start", "end") VALUES ($1, $2);`;
+    let sqlValues = [req.body.start, req.body.end]
+    pool.query(sqlQuery, sqlValues)
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(err => {
+            console.log('error from shiftRouter POST:', err);
             res.sendStatus(500);
         })
 });
