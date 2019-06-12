49ba1bc96bdba2afa25fa3fa3542abddfec98ad4
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-11 21:19:01 -0500

    front end form posts new shift and list re-renders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,11 +5,42 @@ import axios from 'axios';
 class App extends Component {
 
   state = {
-    shiftData: null
+    shiftData: null,
+    newShift: {
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null
+    }
   }
   
   componentDidMount = () => {
     this.fetchShifts();
+  }
+
+  handleChange = property => event => {
+    this.setState({
+      newShift: {
+        ...this.state.newShift,
+        [property]: event.target.value
+      }
+    })
+  }
+
+  handleSubmit = () => {
+    let postData = {
+      start: this.state.newShift.startDate + ' ' + this.state.newShift.startTime,
+      end: this.state.newShift.endDate + ' ' + this.state.newShift.endTime,
+    }
+    axios.post('/api/shift', postData).then(response => {
+      console.log(response.data);
+      if (response.data === 'Created') {
+        alert('success!');
+        this.fetchShifts();
+      } else {
+        alert('failed!');
+      }
+    });
   }
 
   fetchShifts = () => {
@@ -24,19 +55,46 @@ class App extends Component {
   }
 
   render() {
+
+    console.log(this.state);
+
     return (
       <div className="App">
         <header>
-          <h1>When I Might Work</h1>
-          <h2>Mini API</h2>
-          <h3>Built by Thomas Roselyn</h3>
+          <div className="logo"></div>
+          <div className="title">
+            <h1>When I Work</h1>
+            <h2>Mini API</h2>
+            <h3>Built by Thomas Roselyn</h3>
+          </div>
         </header>
         <main>
+          <div className="shiftForm">
+            <input
+              type="date"
+              value={this.state.newShift.startDate}
+              onChange={this.handleChange('startDate')} />
+            <input
+              type="time"
+              value={this.state.newShift.startTime}
+              onChange={this.handleChange('startTime')} />
+            <input
+              type="date"
+              value={this.state.newShift.endDate}
+              onChange={this.handleChange('endDate')} />
+            <input
+              type="time"
+              value={this.state.newShift.endTime}
+              onChange={this.handleChange('endTime')} />
+            <button onClick={this.handleSubmit}>add new shift</button>
+          </div>
+          <div className="shiftList">
             {this.state.shiftData &&
               this.state.shiftData.map(shift => {
                 return <p key={shift.id}>start: {shift.start}, end: {shift.end}</p>
               })
             }
+          </div>
         </main>
       </div>
     );
