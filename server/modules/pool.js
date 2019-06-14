e53eee3867e2a660717e1b5affe949824f6e580c
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-14 06:24:12 -0500

    update to pool config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,29 +3,39 @@ const url = require('url');
 let config = {};
 
 //pool config
-if (process.env.DATABASE_URL) {
-    let params = url.parse(process.env.DATABASE_URL);
-    let auth = params.auth.split(':');
-    config = {
-        user: auth[0],
-        password: auth[1],
-        host: params.hostname,
-        port: params.port,
-        database: params.pathname.split('/')[1],
-        ssl: true,
-        max: 10,
-        idleTimeoutMillis: 30000
-    };
-} else {
-    config = {
-        user: process.env.PG_USER || null,
-        password: process.env.DATABASE_SECRET || null,
-        host: process.env.DATABASE_SERVER || 'localhost',
-        port: process.env.DATABASE_PORT || 5432,
-        database: process.env.DATABASE_NAME || 'wheniwork_mini_api',
-        max: 10,
-        idleTimeoutMillis: 30000,
-    };
-}
+// if (process.env.DATABASE_URL) {
+//     let params = url.parse(process.env.DATABASE_URL);
+//     let auth = params.auth.split(':');
+//     config = {
+//         user: auth[0],
+//         password: auth[1],
+//         host: params.hostname,
+//         port: params.port,
+//         database: params.pathname.split('/')[1],
+//         ssl: true,
+//         max: 10,
+//         idleTimeoutMillis: 30000
+//     };
+// } else {
+//     config = {
+//         user: process.env.PG_USER || null,
+//         password: process.env.DATABASE_SECRET || null,
+//         host: process.env.DATABASE_SERVER || 'localhost',
+//         port: process.env.DATABASE_PORT || 5432,
+//         database: process.env.DATABASE_NAME || 'wheniwork_mini_api',
+//         max: 10,
+//         idleTimeoutMillis: 30000,
+//     };
+// }
+
+config = {
+    user: process.env.PG_USER || null,
+    password: process.env.DATABASE_SECRET || null,
+    host: process.env.DATABASE_SERVER || 'localhost',
+    port: process.env.DATABASE_PORT || 5432,
+    database: process.env.DATABASE_NAME || 'wheniwork_mini_api',
+    max: 10,
+    idleTimeoutMillis: 30000,
+};
 
 module.exports = new pg.Pool(config);
